edd1d99cc7877e320d67700d8647e83cfd09d1f7
Chiemee-Joy <amiablejoyc@gmail.com>
2023-02-21 21:18:30 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,19 +6,15 @@
  */
 void print_alphabet_x10(void);
 {
-	char ch;
-	int i;
+	char c;
+	int i = 0;
 
-	i = 0;
-	while (i < 10)
+	while (i <= 10)
 	{
-		ch = 'a';
-		while (ch <= 'z')
-		{
-			_putchar(ch);
-			ch++;
-		}
-		putchar('\n');
-		i++;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
 	}
+	_putchar('\n');
+	i++;
 }
