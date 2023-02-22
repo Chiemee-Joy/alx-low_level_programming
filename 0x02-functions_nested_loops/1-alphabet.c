c5362833de1fae662fe2a47134af2f70dd076035
Chiemee-Joy <amiablejoyc@gmail.com>
2023-02-21 19:14:36 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	putchar('\n');
+}
