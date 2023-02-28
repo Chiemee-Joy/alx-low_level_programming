804d99ed487c6528ffbc3e752f0ca0246680e548
Chiemee-Joy <amiablejoyc@gmail.com>
2023-02-27 20:14:56 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -26,13 +26,13 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
-			if (d % 2)
+			if (j % 2)
 				digit = -digit;
 			n = n * 10 + digit;
 			k = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-			f = 0;
+			k = 0;
 		}
 		i++;
 	}
