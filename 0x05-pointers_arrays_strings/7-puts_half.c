3782f85fe34c7631483bf8a9ca0a5872e7a74bc2
Chiemee-Joy <amiablejoyc@gmail.com>
2023-02-27 18:26:35 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to print from
+ */
+void puts_half(char *str)
+{
+	int len, n, j;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (j = len / 2; str[j] != '\0'; j++)
+		{
+			_putchar(str[j]);
+		}
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+	_putchar('\n');
+}
