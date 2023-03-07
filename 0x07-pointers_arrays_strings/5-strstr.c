7181599c0def674a2649ee4ce0c17be5f9910690
Chiemee-Joy <amiablejoyc@gmail.com>
2023-03-06 16:24:02 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -23,7 +23,8 @@ char *_strstr(char *haystack, char *needle)
 		{
 			do {
 				if (needle[i + 1] == '\0')
-					i++;
+					return (haystack);
+				i++;
 			} while (haystack[i] == needle[i]);
 		}
 		haystack++;
