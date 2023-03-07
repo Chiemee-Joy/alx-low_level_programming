6dbad6a758d464363e74113e7b2f9c330ec8d98d
Chiemee-Joy <amiablejoyc@gmail.com>
2023-03-06 18:48:21 -0800

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -6,6 +6,7 @@
  * of a square matrix of integers
  * @a: matrix of integers
  * @size: size of the matrix
+ * Return: Always 0 (Success)
  */
 void print_diagsums(int *a, int size)
 {
@@ -15,6 +16,6 @@ void print_diagsums(int *a, int size)
 		sum1 = sum1 + a[i];
 
 	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
-		sum2 = sum2 + a[n];
+	sum2 = sum2 + a[n];
 	prinf("%d, %d\n", sum1, sum2);
 }
